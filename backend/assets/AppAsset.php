ffbffffc6f2b7dfc93301e50312b95c7abbf6380
Shkodenko V. Taras <taras@shkodenko.com>
2020-10-22 18:31:15 +0300

    Added AdminLTE theme

--- backend/assets/AppAsset.php
+++ backend/assets/AppAsset.php
@@ -13,6 +13,8 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         'css/site.css',
+        'css/AdminLTE.min.css',
+        'css/skins/skin-black.min.css',
     ];
     public $js = [
     ];
